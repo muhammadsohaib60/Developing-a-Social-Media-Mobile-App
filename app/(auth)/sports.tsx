cfecf221126e54e0a591bbea8bb7d6a5dfc52453
Gulsher <i222637@nu.edu.pk>
2024-08-25 19:17:39 +0500

    auth flow screens done

--- app/(auth)/sports.tsx
+++ app/(auth)/sports.tsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import Progress from "@/components/Progress";
+import { router } from "expo-router";
+import Header2 from "@/components/Header2";
 
 const Sports = () => {
+  const handleSubmit = () => {
+    router.push("/ethnic");
+  };
+
   return (
-    <View>
-      <Text>Sports</Text>
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <Header2
+        heading1="Choose Your Tribe"
+        heading2="Choose your tribe to start connecting with your favorite Sport Club."
+      />
+      <View
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextInput placeholder="Choose Your Sports Club" style={styles.input} />
+        <TextInput
+          placeholder="Enter Sports Club if not listed"
+          style={styles.input}
+        />
+
+        <CustomButton size={18} text="Next" handlePress={handleSubmit} />
+      </View>
+
+      <Progress activeCircles={3} />
+    </SafeAreaView>
   );
 };
 
 export default Sports;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#4B008290",
+    gap: 40,
+  },
+  input: {
+    backgroundColor: "white",
+    width: "70%",
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    marginBottom: 10,
+    color: "#969696",
+    fontFamily: "ReemRegular",
+  },
+});
